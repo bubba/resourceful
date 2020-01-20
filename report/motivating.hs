5d5e967bd9e19865dfdde1a90e8d8ab2f54c24cc
Luke Lau <luke_lau@icloud.com>
2020-01-20 19:48:03 +0000

    Updates to the introduction

--- report/motivating.hs
+++ report/motivating.hs
@@ -3,3 +3,10 @@ handleRequest conn = do
   a <- readFile "foo.txt"
   b <- receiveData conn
   sendData conn (a <> b)
+
+
+
+writeFile :: IO ()
+readFile :: IO String
+concurrently :: IO a -> IO b -> IO (a, b)
+x = writeFile "foo.txt" `concurrently` readFile "foo.txt"
